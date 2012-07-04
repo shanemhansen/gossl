29be316fef73f376075a4f558de37b36ed44f396
Shane Hansen <shanemhansen@gmail.com>
2012-07-04 01:34:25 -0600

    Benchmarks for aes and sha
    It turns out ssl/aes is slower than go's crypto/aes. Awesome.
    ssl/sha256 is quite a bit faster than go's ssl/sha256

---
+++ src/examples/bench_aes128.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+import "time"
+import "crypto/cipher"
+import "crypto/aes"
+import sslAES "ssl/aes"
+
+func bench(h *cipher.Block, msg string) {
+    bufsize := 102400
+    iterations := 10000
+    src := make([]byte, bufsize)
+    dst := make([]byte, bufsize)
+    t := time.Now()
+    for i:=0; i< iterations; i++ {
+        (*h).Encrypt(src, dst)
+        (*h).Encrypt(dst, src)
+    }
+    fmt.Printf("%s took %s to process %d mb\n", msg, time.Since(t), bufsize*iterations/1024/1024)
+
+}
+func main() {
+    fmt.Println("This program benchmarks crypto/aes and aes/aes implementations")
+    k := []byte("1234567890123456")
+    h, _ := aes.NewCipher(k)
+    bench(&h, "crypto/aes")
+    ssl, _ := sslAES.NewCipher(k)
+    bench(&ssl, "ssl/aes")
+}
---
+++ src/examples/bench_sha256.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+import "time"
+import "hash"
+import "crypto/rand"
+import "crypto/sha256"
+import sslSha256 "ssl/sha256"
+
+func bench(h *hash.Hash, msg string) {
+    bufsize := 10240
+    iterations := 10000
+    buf := make([]byte, bufsize)
+    n, err := rand.Read(buf) 
+    t := time.Now()
+    for i:=0; i< iterations; i++ {
+        if err != nil {
+            panic("problem reading random data")
+        }
+        (*h).Write(buf[0:n])
+    }
+    fmt.Printf("%s took %s to process %d mb\n", msg, time.Since(t), bufsize*iterations/1024/1024)
+
+}
+func main() {
+    fmt.Println("This program benchmarks crypto/sha256 and aes/sha256 implementations")
+    h := sha256.New()
+    bench(&h, "crypto/sha256")
+    ssl := sslSha256.New()
+    bench(&ssl, "ssl/aes")
+}
