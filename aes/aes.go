6675938785c58435ef91c0061b7ce182a3ad6bf9
Vincent Batts <vbatts@redhat.com>
2014-06-09 15:09:27 -0400

    aes: cleanup, robustness, and upstream alignment
    * a little cleanup to have the API mimic upstream crypto/aes
    * adding error handling to be more informative and cover edgecases
    * including the upstream tests (relevant pieces)
    * including a gocompat build tag, to mimic behaviour of go crypto/aes

--- aes/aes.go
+++ aes/aes.go
@@ -16,46 +16,105 @@ import (
 	"errors"
 	"strconv"
 	"unsafe"
+
+	"github.com/vbatts/gossl/sslerr"
 )
 
 const BlockSize = 16
 
-type AESKey struct {
+type aesKey struct {
 	_aes_encrypt_key C.AES_KEY
 	_aes_decrypt_key C.AES_KEY
 }
 
 var (
-	errorSettingKey = errors.New("problem setting key")
+	errorSettingKey   = errors.New("problem setting key")
+	errorWrongSizeKey = errors.New("key length not valid")
+	errorInvalidKey   = errors.New("initial keys are not valid")
 )
 
 //Construct a new instance. see crypto/aes
 // returning a block cipher that satisfies the crypto/cipher.Block interface
 func NewCipher(key []byte) (cipher.Block, error) {
-	aes := new(AESKey)
+	aes := new(aesKey)
 	key_p := pointerFromBytes(key)
 	key_bits := C.int(len(key) * 8)
-	if C.AES_set_encrypt_key(key_p, key_bits, &aes._aes_encrypt_key) != 0 {
+	if ret := C.AES_set_encrypt_key(key_p, key_bits, &aes._aes_encrypt_key); ret != 0 {
+		// in case the error is something deep in OpenSSL land
+		if err := sslerr.Error(); err != nil {
+			return aes, err
+		}
+		if ret == -1 {
+			return aes, errorInvalidKey
+		}
+		if ret == -2 {
+			return aes, errorWrongSizeKey
+		}
+		// standard fallback error
 		return aes, errorSettingKey
 	}
-	if C.AES_set_decrypt_key(key_p, key_bits, &aes._aes_decrypt_key) != 0 {
+	if ret := C.AES_set_decrypt_key(key_p, key_bits, &aes._aes_decrypt_key); ret != 0 {
+		// in case the error is something deep in OpenSSL land
+		if err := sslerr.Error(); err != nil {
+			return aes, err
+		}
+		if ret == -1 {
+			return aes, errorInvalidKey
+		}
+		if ret == -2 {
+			return aes, errorWrongSizeKey
+		}
+		// standard fallback error
 		return aes, errorSettingKey
 	}
 	return aes, nil
 }
+
 func pointerFromBytes(b []byte) *C.uchar {
 	return (*C.uchar)(unsafe.Pointer(&b[0]))
 }
-func (self *AESKey) BlockSize() int { return BlockSize }
-func (self *AESKey) Encrypt(dst, src []byte) {
+func (self *aesKey) BlockSize() int { return BlockSize }
+func (self *aesKey) Encrypt(dst, src []byte) {
+	if goCompatible {
+		// this something the golang crypto/aes does, that does not happen for OpenSSL.
+		if len(src) < BlockSize {
+			panic("crypto/aes: input not full block")
+		}
+		if len(dst) < BlockSize {
+			panic("crypto/aes: output not full block")
+		}
+	}
+
 	dst_p := pointerFromBytes(dst)
 	src_p := pointerFromBytes(src)
 	C.AES_ecb_encrypt(src_p, dst_p, &self._aes_encrypt_key, C.AES_ENCRYPT)
+
+	// for safe measure
+	err := sslerr.Error()
+	if err != nil {
+		panic(err.Error())
+	}
 }
-func (self *AESKey) Decrypt(dst, src []byte) {
+func (self *aesKey) Decrypt(dst, src []byte) {
+	if goCompatible {
+		// this something the golang crypto/aes does, that does not happen for OpenSSL.
+		if len(src) < BlockSize {
+			panic("crypto/aes: input not full block")
+		}
+		if len(dst) < BlockSize {
+			panic("crypto/aes: output not full block")
+		}
+	}
+
 	dst_p := pointerFromBytes(dst)
 	src_p := pointerFromBytes(src)
 	C.AES_ecb_encrypt(src_p, dst_p, &self._aes_decrypt_key, C.AES_DECRYPT)
+
+	// for safe measure
+	err := sslerr.Error()
+	if err != nil {
+		panic(err.Error())
+	}
 }
 
 // Here for compatibility with 'crypto/aes'
